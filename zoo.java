5e7a386ca49c0f0b6da12dc676cc1c84e2af854b
liceal <675024132@qq.com>
2018-09-25 08:24:35 +0800

    liceal的动物园  请来添加自己的动物

---
+++ zoo.java
@@ -0,0 +1,15 @@
+public class Rabbit{
+    private name;
+    private type;
+    public void setRabbit(string name,string type){
+        this.name=name;
+        this.type=type;
+    }
+}
+public class  Zoo{
+    public static main(){
+
+    }
+}
+
+
