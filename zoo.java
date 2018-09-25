2bac464c52f157a26d348c6bb0d2d8e5d8561e66
Administrator <627831579@qq.com>
2018-09-25 08:30:15 +0800

    dasadasdsaa

--- zoo.java
+++ zoo.java
@@ -6,6 +6,14 @@ public class Rabbit{
         this.type=type;
     }
 }
+public class Sheep{
+    private name;
+    private type;
+    public void setSheep(string name,string type){
+        this.name=name;
+        this.type=type;
+    }
+}
 public class  Zoo{
     public static main(){
 
