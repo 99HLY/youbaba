7f9be7b34ba8847ecc9d1d35cbe058adcf1435ab
胡益鑫 <000>
2018-09-27 10:48:52 +0800

    胡益鑫的提交

--- huyixin.php
+++ huyixin.php
@@ -2,3 +2,4 @@
 echo"hyx的测试";
 echo "index默认文件 huyixin的修改 请你们进行接下来的修改 跟我一样"."<br/>";
 ?>
+
--- liuzhijie.php
+++ liuzhijie.php
@@ -4,4 +4,6 @@ echo "index默认文件 liuzhijie的修改 请你们进行接下来的修改 跟
 echo"99HLY的测试";
 echo "index默认文件 99HLY的修改 请你们进行接下来的修改 跟我一样"."<br/>";
 echo"liceal的测试";
+echo "index默认文件 99HLY的修改 请你们进行接下来的修改 跟我一样"."<br/>";
+echo "huyix的测试"
 ?>
